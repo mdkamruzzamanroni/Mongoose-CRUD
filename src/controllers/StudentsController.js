db1cd60fcb50598a45f5c5b4838dc4ed4e65f822
Md Kamruzzaman <kamruzzaman.dte@gmail.com>
2022-11-18 23:23:33 +0600

    2nd commit

--- src/controllers/StudentsController.js
+++ src/controllers/StudentsController.js
@@ -31,3 +31,30 @@ exports.ReadStudent=(req,res)=>{
 }
 
 //U = UPDATE
+exports.UpdateStudent=(req,res)=>{
+let id=req.params.id;
+let Query={_id:id}
+let reqBody=req.body;
+StudentsModel.updateOne(Query,reqBody,(error,data)=>{
+    if(error){
+        res.status(400).json({status:"fail",data:error})
+    }
+   else{
+        res.status(200).json({status:"success",data:data})
+       }
+})
+}
+//D= DELETE
+exports.DeleteStudent=(req,res)=>{
+    let id=req.params.id;
+let Query={_id:id}
+StudentsModel.remove(Query,(error,data)=>{
+    if(error){
+        res.status(400).json({status:"fail",data:error})
+    }
+   else{
+        res.status(200).json({status:"success",data:data})
+       }
+
+})
+}
--- src/routes/api.js
+++ src/routes/api.js
@@ -14,6 +14,8 @@ router.post("/hello-post",HelloController.HelloPost)
 
 router.post("/InsertStudent",StudentsController.InsertStudent)
 router.get("/ReadStudent",StudentsController.ReadStudent)
+router.post("/UpdateStudent/:id",StudentsController.UpdateStudent)
+router.get("/DeleteStudent/:id",StudentsController.DeleteStudent)
 
 module.exports=router;
 
